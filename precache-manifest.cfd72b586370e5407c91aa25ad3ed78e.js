dadb7881aad4d12147e0373da658832247b450c9
Joe Nyzio <jdnyzio@gmail.com>
2021-10-07 21:56:24 -0700

    Updates

---
+++ precache-manifest.cfd72b586370e5407c91aa25ad3ed78e.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "3580d3cfeb7905a18f1a",
+    "url": "/social-network/static/css/main.c6b73e3d.chunk.css"
+  },
+  {
+    "revision": "3580d3cfeb7905a18f1a",
+    "url": "/social-network/static/js/main.3580d3cf.chunk.js"
+  },
+  {
+    "revision": "106aa48fd46426e4ee50",
+    "url": "/social-network/static/css/1.9d1f1d7b.chunk.css"
+  },
+  {
+    "revision": "106aa48fd46426e4ee50",
+    "url": "/social-network/static/js/1.106aa48f.chunk.js"
+  },
+  {
+    "revision": "22e660ce69f7e2ad833f",
+    "url": "/social-network/static/js/runtime~main.22e660ce.js"
+  },
+  {
+    "revision": "81b6b7abf1a0a2bb2eb664e89d44d40e",
+    "url": "/social-network/index.html"
+  }
+];
